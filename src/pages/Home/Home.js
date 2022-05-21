6721e47857d98b3d9d35e3913fe37a17976bdcbf
Alexandre-st <alexandre.saintprix@gmail.com>
2022-05-21 11:34:21 +0200

    Add About part in the homepage, full responsive

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,17 @@
+import { Link } from 'react-router-dom';
+
 import HeroImg from '../../assets/homepage/mobile/image-homepage-hero.jpg';
 import HeroImgTablet from '../../assets/homepage/tablet/image-homepage-hero.jpg';
 import HeroImgDesktop from '../../assets/homepage/desktop/image-homepage-hero.jpg';
+import AboutImg from '../../assets/homepage/mobile/image-homepage-profile.jpg';
+import AboutImgTablet from '../../assets/homepage/tablet/image-homepage-profile.jpg';
+import AboutImgDesktop from '../../assets/homepage/desktop/image-homepage-profile.jpg';
 import StyleButton from '../../assets/icons/down-arrows.svg';
 
 const Home = () => {
   return ( 
     <main className="container">
+
       <section className="hero">
         <div className="hero-content">
           <picture className="hero-img">
@@ -25,6 +31,29 @@ const Home = () => {
               </div>
             </div>
           </picture>
+        </div>
+      </section>
+
+      <section className="about">
+        <div className="about-container">
+          <picture className="about-img">
+            <source srcSet={AboutImgDesktop} media="(min-width: 1024px)" />
+            <source srcSet={AboutImgTablet}  media="(min-width: 768px)" />
+            <img src={AboutImg} alt="My profil" />
+          </picture>
+          <div className="about-content">
+            <div className="border"></div>
+            <div className="about-content-text">
+              <h2 className="middle-title">About Me</h2>
+              <p className="text">
+                I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in London, UK, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work.
+              </p>
+              <div className="button button-white" role="button">
+                <Link to="/">GO TO PORTFOLIO</Link>
+              </div>
+            </div>
+            <div className="border"></div>
+          </div>
         </div>
       </section>
     </main>
