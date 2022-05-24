37a7991fe85ed03896d061cf33ff0fe4c104ed1b
Alexandre-st <alexandre.saintprix@gmail.com>
2022-05-24 15:54:11 +0200

    Add the form contact, working with validation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,4 @@
+import { useFormik } from 'formik';
 
 import GithubImg from '../assets/icons/github.svg';
 import TwitterImg from '../assets/icons/twitter.svg';
@@ -5,6 +6,37 @@ import LinkedinImg from '../assets/icons/linkedin.svg';
 
 
 const Contact = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: ''
+    },
+    validate : values => {
+      const errors = {}; 
+      if (!values.name) {
+        errors.name = "This field is required";
+      }
+      if (!values.email) {
+        errors.email = "This field is required";
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Please use a valid email address';
+      }
+      if (!values.message) {
+        errors.message = "This field is required";
+      }
+      return errors;
+    }, 
+    onSubmit: (values, { setSubmitting }) => { 
+      console.log("submit", values);
+      setSubmitting(false);
+      formik.resetForm({ values : ''});
+    }
+  });
+
+  console.log(formik)
+  
   return ( 
     <main className="container">
       <section className="infos">
@@ -28,20 +60,44 @@ const Contact = () => {
       <section className="contact">
         <div className="contact-container">
           <h2 className="middle-title">Contact Me</h2>
-          <form className="contact-form">
+          <form className="contact-form" onSubmit={formik.handleSubmit} >
             <div className="contact-form-input">
               <label className="contact-form-label" htmlFor="name">Name</label>
-              <input type="text" placeholder="Jane Appleseed" />
+              <input 
+                type="text"
+                className={`${formik.touched.name && formik.errors.name ? "input-error" : ""}`}
+                placeholder="Jane Appleseed"
+                {...formik.getFieldProps("name")}                  
+              />
+              {formik.touched.name && formik.errors.name && <p className="contact-form-error">{formik.errors.name}</p>}
             </div>
             <div className="contact-form-input">
               <label className="contact-form-label" htmlFor="email">Email Adress</label>
-              <input type="text" placeholder="email@example.com" />
+              <input 
+                type="text"
+                className={`${formik.touched.email && formik.errors.email ? "input-error" : ""}`}
+                placeholder="email@example.com"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && formik.errors.email && <p className="contact-form-error">{formik.errors.email}</p>}
             </div>
             <div className="contact-form-area">
               <label className="contact-form-label" htmlFor="message">Message</label>
-              <textarea type="text" placeholder="How can I help?" rows={3} />
+              <textarea 
+                type="text" 
+                className={`${formik.touched.message && formik.errors.message ? "area-error" : ""}`}
+                placeholder="How can I help?" {...formik.getFieldProps("message")} 
+                rows={3} 
+              />
+              {formik.touched.message && formik.errors.message && <p className="contact-form-error">{formik.errors.message}</p>}
             </div>
-            <button className="button button-form" type="submit">SEND MESSAGE</button>
+            <button 
+              className="button button-form" 
+              disabled={!(formik.isValid && formik.dirty)} 
+              type="submit"
+            >
+              SEND MESSAGE
+            </button>
           </form>
         </div>
       </section>
